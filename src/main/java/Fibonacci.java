c2732d3df84317058f41feed93b935a7194cd494
Bohdan Raman <bohdanraman@gmail.com>
2022-09-29 14:32:27 +0300

    add class fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,5 +1,14 @@
 public class Fibonacci {
     public void getNumbersFibonacci(int a) {
-
+        /*A Fibonacci sequence is the sequence of numbers 1, 1, 2, 3, 5, 8, 13, 21, 34, 55...*/
+        int b1 = 1;
+        int b2 = 1;
+        int nextNumber;
+        for (int i = 0; i < a; i++) {
+            nextNumber = b1 + b2;
+            System.out.println("number a Fibonacci = " +nextNumber);
+            b1 = b2;
+            b2 = nextNumber;
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Task 4");
         CycleProgram cycleProgram = new CycleProgram();
         cycleProgram.getPrimeNumbers();
+        System.out.println();
 
+        System.out.println("Task 9");
+        Fibonacci fibonacci = new Fibonacci();
+        fibonacci.getNumbersFibonacci(5);
 
+        }
     }
-}
