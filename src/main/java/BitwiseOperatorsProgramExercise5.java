19db12f71be6c4402817df156c0d6a6f0f49f21c
Bohdan Raman <bohdanraman@gmail.com>
2022-11-21 21:10:52 +0300

    refactor BitwiseOperatorsProgramExercise5

--- src/main/java/BitwiseOperatorsProgramExercise5.java
+++ src/main/java/BitwiseOperatorsProgramExercise5.java
@@ -23,19 +23,15 @@ public class BitwiseOperatorsProgramExercise5 {
     }
 
 
-    public static void binar(int a) {
+    public static void getBinary(int a) {
         int b;
         String temp = "";
         while (a != 0) {
             b = a % 2;
-            temp = b + temp;
             a = a / 2;
+            temp = b + temp;
         }
         System.out.print(temp);
-    }
-
-    public static void main(String[] args) {
-        binar(89);
     }
 }
 
